06519d6786766034f4f31bc1b7c97183b241daac
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-10-13 12:38:50 +0200

    Scope notation

--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -1,18 +1,31 @@
 package com.bolsadeideas.springboot.di.app.models.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 @Component
+//ya no es singleton, este beans va a durar lo que dura una peticion http por lo tanto cada usuario que se conecte va a tener una factura distinta y propia 
+@RequestScope
+//para que la sesion http sea persistente has que cerremos navegador
+//hay que implementar serializable pq convierte objeto en secuencia de bytes
+//@SessionScope
 
-public class Factura {
+public class Factura implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 946004357128146951L;
+
 	@Value("${factura.descripcion}")
 	private String descripcion;
 
